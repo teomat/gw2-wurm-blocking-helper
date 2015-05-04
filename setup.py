e30d398ff07ac2ee8f90391a38b829eb3cdf0a2e
Matteo Goggi <matteo.goggi@gmail.com>
2015-05-04 17:30:13 +0200

    exclude some unneeded modules when building.

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['wurm overlay.py'], options={"py2exe":{"includes":["sip"]}})
+py2exe_options = dict(
+	excludes=['doctest', 'pdb', 'unittest', 'difflib', 'inspect'],
+	includes=['sip'],
+	compressed=True
+)
+
+setup(windows=['wurm overlay.py'], options={"py2exe": py2exe_options})

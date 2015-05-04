472ec05ab78f52378d6a4232901a49d9521093ff
Matteo Goggi <matteo.goggi@gmail.com>
2015-05-04 14:26:22 +0200

    add build script, fix setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['wurm overlay.pyw'], options={"py2exe":{"includes":["sip"]}})
+setup(windows=['wurm overlay.py'], options={"py2exe":{"includes":["sip"]}})
